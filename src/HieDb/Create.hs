0d330f0c27d2d8c5b296ca99ec546c84b271526a
Zubin Duggal <zubin@cmi.ac.in>
2020-05-03 13:01:32 +0530

    allow adding refs from an in-memory hiefile

--- src/HieDb/Create.hs
+++ src/HieDb/Create.hs
@@ -17,6 +17,7 @@ import Control.Monad.IO.Class
 import System.Directory
 
 import Database.SQLite.Simple
+import Data.Time.Clock
 
 import HieDb.Types
 import HieDb.Utils
@@ -31,22 +32,25 @@ initConn (getConn -> conn) = do
   execute_ conn "CREATE TABLE IF NOT EXISTS decls (hieFile TEXT, mod TEXT, unit TEXT, occ TEXT, file TEXT, sl INTEGER, sc INTEGER, el INTEGER, ec INTEGER, is_root BOOL)"
 
 addRefsFrom :: (MonadIO m, NameCacheMonad m) => HieDb -> FilePath -> m ()
-addRefsFrom (getConn -> conn) path = do
+addRefsFrom c@(getConn -> conn) path = do
   time <- liftIO $ getModificationTime path
   mods <- liftIO $ query conn "SELECT * FROM mods WHERE hieFile = ? AND time >= ?" (path, time)
   case mods of
     (HieModuleRow{}:_) -> return ()
-    [] -> withHieFile path $ \hf -> liftIO $ withTransaction conn $ do
-      execute conn "DELETE FROM refs WHERE src = ?" (Only path)
-      execute conn "DELETE FROM decls WHERE hieFile = ?" (Only path)
-      let mod = moduleName $ hie_module hf
-          uid = moduleUnitId $ hie_module hf
-          modrow = HieModuleRow path mod uid time
-      execute conn "INSERT INTO mods VALUES (?,?,?,?)" modrow
-      let rows = genRefRow path hf
-      executeMany conn "INSERT INTO refs VALUES (?,?,?,?,?,?,?,?,?,?,?)" rows
-      let decls = genDeclRow path hf
-      executeMany conn "INSERT INTO decls VALUES (?,?,?,?,?,?,?,?,?,?)" decls
+    [] -> withHieFile path $ \hf -> addRefsFromLoaded c path time hf
+
+addRefsFromLoaded :: (MonadIO m) => HieDb -> FilePath -> UTCTime -> HieFile -> m ()
+addRefsFromLoaded (getConn -> conn) path time hf = liftIO $ withTransaction conn $ do
+  execute conn "DELETE FROM refs WHERE src = ?" (Only path)
+  execute conn "DELETE FROM decls WHERE hieFile = ?" (Only path)
+  let mod = moduleName $ hie_module hf
+      uid = moduleUnitId $ hie_module hf
+      modrow = HieModuleRow path mod uid time
+  execute conn "INSERT INTO mods VALUES (?,?,?,?)" modrow
+  let rows = genRefRow path hf
+  executeMany conn "INSERT INTO refs VALUES (?,?,?,?,?,?,?,?,?,?,?)" rows
+  let decls = genDeclRow path hf
+  executeMany conn "INSERT INTO decls VALUES (?,?,?,?,?,?,?,?,?,?)" decls
 
 deleteFileFromIndex :: HieDb -> FilePath -> IO ()
 deleteFileFromIndex (getConn -> conn) path = liftIO $ withTransaction conn $ do
