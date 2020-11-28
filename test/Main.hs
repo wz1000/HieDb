77ab67bbe3fe6c24057f48110e9906d215034550
Jan Hrček <honza.hrk@gmail.com>
2020-11-28 16:47:11 +0100

    Start some api tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,60 +1,91 @@
 module Main where
 
+import GHC.Paths (libdir)
+import HieDb (HieDb, HieModuleRow (..), ModuleInfo (..), getAllIndexedMods, withHieDb')
+import HieDb.Run (Command (..), Options (..), runCommand)
+import Module (moduleNameString)
 import System.Directory (findExecutable, getCurrentDirectory, removeDirectoryRecursive)
 import System.Exit (ExitCode (ExitSuccess), die)
 import System.FilePath ((</>))
 import System.Process (callProcess, proc, readCreateProcessWithExitCode)
-import Test.Hspec (Expectation, Spec, afterAll_, beforeAll_, describe, hspec, it, shouldBe)
+import Test.Hspec (Expectation, Spec, afterAll_, around, beforeAll_, describe, hspec, it, shouldBe)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
-  describe "hiedb (command line)" $ do
-    beforeAll_ compileTestModules $ afterAll_ cleanTestData $ do
-      describe "index" $
-        it "indexes testing project .hie files" $ do
-          runHieDbCli ["index", testTmp, "--quiet"]
-            `suceedsWithStdin` ""
+spec =
+  describe "hiedb" $
+    beforeAll_ compileTestModules $
+    afterAll_ cleanTestData $ do
+      cliSpec
+      apiSpec
 
-      describe "ls" $
-        it "lists the indexed modules" $ do
-          cwd <- getCurrentDirectory
-          runHieDbCli ["ls"]
-            `suceedsWithStdin` unlines (fmap (\x -> cwd </> testTmp </> x)
-                [ "Foo.hie\tFoo\tmain"
-                , "One/Two/Some.hie\tOne.Two.Some\tmain"
-                ])
+apiSpec :: Spec
+apiSpec = describe "api" $
+  beforeAll_ (runCommandTest (Index [testTmp])) $
+    around withTestDb $
+      describe "HieDb.Query" $ do
+        it "getAllIndexedMods returns all indexed modules" $ \conn -> do
+          mods <- getAllIndexedMods conn
+          case mods of
+            [m1,m2] -> do
+              moduleNameString (modInfoName (hieModInfo m1)) `shouldBe` "Foo"
+              moduleNameString (modInfoName (hieModInfo m2)) `shouldBe` "One.Two.Some"
+            xs -> fail $ "Was expecting 2 modules, but got " <> show (length xs)
 
-      describe "name-refs" $
-        it "lists all references of given function" $ do
-          runHieDbCli ["name-refs", "printInt"]
-            `suceedsWithStdin` unlines
-              ["Foo:3:7-3:15"
-              ,"Foo:12:1-12:9"
-              ,"Foo:13:1-13:9"
-              ]
+
+cliSpec :: Spec
+cliSpec =
+  describe "Command line" $ do
+    describe "index" $
+      it "indexes testing project .hie files" $ do
+        runHieDbCli ["index", testTmp, "--quiet"]
+          `suceedsWithStdin` ""
+
+    describe "ls" $
+      it "lists the indexed modules" $ do
+        cwd <- getCurrentDirectory
+        let expectedOutput = unlines (fmap (\x -> cwd </> testTmp </> x)
+              [ "Foo.hie\tFoo\tmain"
+              , "One/Two/Some.hie\tOne.Two.Some\tmain"
+              ])
+        runHieDbCli ["ls"] `suceedsWithStdin` expectedOutput
+
+    describe "name-refs" $
+      it "lists all references of given function" $ do
+        runHieDbCli ["name-refs", "printInt"]
+          `suceedsWithStdin` unlines
+            ["Foo:3:7-3:15"
+            ,"Foo:12:1-12:9"
+            ,"Foo:13:1-13:9"
+            ]
+
+    describe "point-refs" $
+      it "list references at given point" $
+        runHieDbCli ["point-refs", "Foo", "13", "2"]
+          `suceedsWithStdin` unlines
+            [ "Name printInt at (13,2) is used in:"
+            , "Foo:3:7-3:15"
+            , "Foo:12:1-12:9"
+            , "Foo:13:1-13:9"
+            ]
 
 
 suceedsWithStdin :: IO (ExitCode, String, String) -> String -> Expectation
-suceedsWithStdin action expectedStdin =
-  succeedsWithStdinSatisfying action (`shouldBe`expectedStdin)
-
-
-succeedsWithStdinSatisfying :: IO (ExitCode, String, String) -> (String -> Expectation) -> Expectation
-succeedsWithStdinSatisfying action expectStdin  = do
+suceedsWithStdin action expectedStdin = do
   (exitCode, actualStdin, actualStdErr) <- action
   exitCode `shouldBe` ExitSuccess
   actualStdErr `shouldBe` ""
-  expectStdin actualStdin
+  actualStdin `shouldBe` expectedStdin
 
 
 runHieDbCli :: [String] -> IO (ExitCode, String, String)
 runHieDbCli args = do
   hiedb <- findHieDbExecutable
-  let createProc = proc hiedb ("--database" : testDb : args)
-  putStrLn $ unwords $ "RUNNING:":hiedb:args
+  let argsWithTestDb = "--database" : testDb : args
+  let createProc = proc hiedb argsWithTestDb
+  putStrLn $ unwords $ "RUNNING:" : argsWithTestDb
   readCreateProcessWithExitCode createProc ""
 
 
@@ -87,3 +118,17 @@ testDb = testTmp </> "test.hiedb"
 
 testTmp :: FilePath
 testTmp = "test/tmp"
+
+withTestDb :: (HieDb -> IO a) -> IO a
+withTestDb = withHieDb' libdir testDb
+
+runCommandTest :: Command -> IO ()
+runCommandTest = runCommand libdir testOpts
+
+testOpts :: Options
+testOpts = Options
+  { database = testDb
+  , trace = False
+  , quiet = True
+  , virtualFile = False
+  }
